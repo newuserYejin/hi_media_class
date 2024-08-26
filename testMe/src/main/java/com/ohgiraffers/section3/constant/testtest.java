a7969e7d77556dd70e3eceb4db9431244f95c66d
yejin <jin200126@gmail.com>
2024-08-26 15:56:05 +0900

    [히얏]

--- testMe/src/main/java/com/ohgiraffers/section3/constant/testtest.java
+++ testMe/src/main/java/com/ohgiraffers/section3/constant/testtest.java
@@ -2,6 +2,6 @@ package com.ohgiraffers.section3.constant;
 
 public class testtest {
     public static void main(String[] args) {
-        System.out.println("호호");
+        System.out.println("히얏");
     }
 }
