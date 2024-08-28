6584cd113d4f169176979883f26b75ebdf0fe338
yejin <jin200126@gmail.com>
2024-08-28 18:16:58 +0900

    update

--- chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Home.java
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Home.java
@@ -3,26 +3,23 @@ package com.ohgiraffers.section01.method;
 public class Home {
 
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 2;
+        int a = 5;  // 8월 10일 기준
+        int b = 0;
+        boolean A = a > 8 || a+b>=8 ? true : false ;
+        boolean B = b>=4 ? true : false ;
 
-        Home home = new Home();
+        System.out.println("8/18==========");
+        System.out.println("A패키지: "+ a + "/8" + "출발 " + (A?"가능":"불가능"));
+        System.out.println("B패키지: "+ b + "/4" + "출발 " + (B?"가능":((a+b)>=8)?"A회사에 합류해서 가능":"불가능"));
 
-        home.add(num1,num2);
-    }
+        System.out.println("8/20==========");
+        b = 4;
 
-    public void add (int a, int b) {
-        int c = a + b;
-        int d = b++;
+        A = a > 8 || a+b>=8 ? true : false ;
+        B = b>=4 ? true : false ;
 
-        sub(c,d);
+        System.out.println("A패키지: "+ a + "/8" + "출발 " + (A?"가능":"불가능"));
+        System.out.println("B패키지: "+ b + "/4" + "출발 " + (B?"가능":((a+b)>=8)?"A회사에 합류해서 가능":"불가능"));
 
-        System.out.println("결과: " + c);
-
-        sub(a,b);
-    }
-
-    public void sub (int a, int b) {
-        System.out.println("결과: " + (a - b));
     }
 }
